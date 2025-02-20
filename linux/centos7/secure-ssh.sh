37a39df981fde6173c7e44a43dc8a7d13dbe814a
louismattiolo <130078590+louismattiolo@users.noreply.github.com>
2025-02-19 20:19:04 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,10 +3,11 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes root ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
-useradd -m -d /home/$1 -s /bin/bash $1
-mkdir /home/$1/.ssh
-cp /home/louis/SYS265/docker01/SYS-Admin-2/linux/centos7/id_rsa.pub /home/$1/.ssh/authorized_keys
-chmod 700 /home/$1/.ssh
-chmod 600 /home/$1/.ssh/authorized_keys
-chown -R $1:$1 /home/$1/.ssh
+echo -n "Enter user: "
+read user
+useradd -m -d /home/$user -s /bin/bash $user
+mkdir /home/$user/.ssh
+cp /home/louis/SYS265/docker01/SYS-Admin-2/linux/centos7/id_rsa.pub /home/$user/.ssh/authorized_keys
+chmod 700 /home/$user/.ssh
+chmod 600 /home/$user/.ssh/authorized_keys
+chown -R $user:$user /home/$user/.ssh
