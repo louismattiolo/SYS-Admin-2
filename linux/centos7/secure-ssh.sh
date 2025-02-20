97b9cc3ae24a93bccdfbc9fbc89599c618877588
louismattiolo <130078590+louismattiolo@users.noreply.github.com>
2025-02-19 20:10:21 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -6,7 +6,7 @@
 echo "ALL YOUR CODE GOES HERE"
 useradd -m -d /home/$1 -s /bin/bash $1
 mkdir /home/$1/.ssh
-cp /home/louis/SYS-ADMIN-2/linux/centos7/id_rsa.pub /home/$1/.ssh/authorized_keys
+cp /home/louis/SYS265/docker01/SYS-Admin-2/linux/centos7/id_rsa.pub /home/$1/.ssh/authorized_keys
 chmod 700 /home/$1/.ssh
 chmod 600 /home/$1/.ssh/authorized_keys
 chown -R $1:$1 /home/$1/.ssh
