7b98d06b24e99be86e2f27806953b220c962324b
louismattiolo <130078590+louismattiolo@users.noreply.github.com>
2025-02-19 20:07:08 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,9 +4,9 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes root ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
-sudo useradd -m -d /home/$1 -s /bin/bash $1
-sudo mkdir /home/$1/.ssh
-sudo cp /home/louis/SYS-ADMIN-2/linux/centos7/id_rsa.pub /home/$1/.ssh/authorized_keys
-sudo chmod 700 /home/$1/.ssh
-sudo chmod 600 /home/$1/.ssh/authorized_keys
-sudo chown -R $1:$1 /home/$1/.ssh
+useradd -m -d /home/$1 -s /bin/bash $1
+mkdir /home/$1/.ssh
+cp /home/louis/SYS-ADMIN-2/linux/centos7/id_rsa.pub /home/$1/.ssh/authorized_keys
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1 /home/$1/.ssh
